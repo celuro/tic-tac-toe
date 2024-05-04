8e356cd0df1e692f8b777df61cdaa97aa87f0600
mi <144652524+celuro@users.noreply.github.com>
2024-05-04 12:57:44 -0400

    Create gameState.cpp

---
+++ src/game-state/gameState.cpp
@@ -0,0 +1,37 @@
+/******************************************************************************
+* @file gameState.cpp
+* @brief Helper function for the game loop
+*
+* Purpose: This file manages player names and ending the game
+*
+* @author mimikyumom.
+* @bug No known bugs
+******************************************************************************/
+
+#include "../../include/game-state/gameState.h"
+
+std::string playerNameInput(std::string player)
+{
+  std::cout << "Enter the name of " << player << ": ";
+  std::string name;
+  std::cin >> name;
+  return name;
+}
+
+bool manageInput(Player& player, Board& gameBoard)
+{
+  std::cout << "Player " << player.getPlayerID() << " input: ";
+  gameBoard.playerInput(player.getPlayerID());
+  gameBoard.displayBoard();
+
+  // If a player won or they both tied we will commmunicate that the game ends
+  bool gameFinished = gameBoard.didPlayerWin(player.getName());
+  bool gameTie = gameBoard.checkPossibleTie();
+
+  if (gameFinished || gameTie) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
